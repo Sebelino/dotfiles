171e1c19275c68a8d35a50563b13ed50fa20a81e
Sebastian Olsson <sebastian.olsson@tink.se>
2018-10-13 21:29:34 +0200

    Fixed siggraph bug

--- scripts/siggraph.sh
+++ scripts/siggraph.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+# Deps:
+# * graphviz
+# * https://github.com/bmhm/sig2dot2
+
 dotfile="/tmp/keys_temp.dot"
 outfile="/tmp/keys_temp.png"
 
+set -x
 
 gpg --no-options --with-colons --fixed-list-mode --list-sigs | python "$HOME/lib/sig2dot2/sig2dot/sig2dot.py" -u "[User ID not found]" > "$dotfile"
 
-dot -Tpng > "$1"
-
-sig2png.sh "$outfile"
+dot -Tpng "$dotfile" > "$outfile"
 
 feh "$outfile"
 
